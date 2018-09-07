0e7d6b3a10f950da4d353d92f2d143529c22db1b
Freddy Ruben <freddyruben@outlook.com>
2018-09-07 13:35:50 -0500

    Modificado algunos elementos del header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,12 @@ import { Http } from '@angular/http';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'ikusi';
   Users : Array<Users>;
+  
 
   constructor(private http: Http){
     this.http.get('http://jsonplaceholder.typicode.com/users').subscribe(resp => this.Users = resp.json());

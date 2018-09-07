81e13cf6178d1839abea0a3f356dd8f1f900f3c9
Xavi Morales <jamoco_1990@hotmail.com>
2018-09-07 13:25:41 -0500

    Login pero no desde el servidor 2

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import { NgForm } from '@angular/forms';
+
 
 
 @Component({
@@ -9,12 +11,21 @@ import {Router} from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router:Router) { }
-
+ 
+  constructor( private router:Router){}
   ngOnInit() {
   }
   handleClick() {
-    this.router.navigate(['inicio']);
+    
+  }
+  /* En esta parte estamos validando los datos para poder loguearnos */
+  login(form: NgForm){
+    console.log(form.value);
+    if(form.value.usuario==='Admin' && form.value.password==='Admin')
+    {
+      localStorage.setItem('Admin', form.value.usuario);
+      this.router.navigate(['inicio']);
+    }
   }
 
 }
--- src/app/my-nav/my-nav.component.ts
+++ src/app/my-nav/my-nav.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { Users } from '../models/users';
+import { Http } from '@angular/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-my-nav',
@@ -9,12 +12,22 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./my-nav.component.css']
 })
 export class MyNavComponent {
+  Users : Array<Users>;
+
+  constructor(private breakpointObserver: BreakpointObserver, private http: Http, private router: Router) {
+    this.http.get('http://jsonplaceholder.typicode.com/users').subscribe(resp => this.Users = resp.json());
+  }
+
+  logout(){
+    localStorage.removeItem('Admin');
+    this.router.navigate(['']);
+  }
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
     
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  
   
   }
